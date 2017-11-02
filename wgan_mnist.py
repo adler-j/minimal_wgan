299d1a6daf819b7eebf1dd9e59994618cb1fc6be
Jonas Adler <jonasadl@kth.se>
2017-11-02 03:31:42 +0100

    Style improvements

--- wgan_mnist.py
+++ wgan_mnist.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 from tensorflow.contrib import layers
 
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+mnist = input_data.read_data_sets('MNIST_data')
 
 session = tf.InteractiveSession()
 
@@ -19,12 +19,10 @@ def generator(z):
         z = tf.reshape(z, [-1, 4, 4, 256])
 
         z = layers.conv2d_transpose(z, num_outputs=128, kernel_size=5, stride=2)
-        z = z[:, :7, :7, :]
         z = layers.conv2d_transpose(z, num_outputs=64, kernel_size=5, stride=2)
         z = layers.conv2d_transpose(z, num_outputs=1, kernel_size=5, stride=2,
                                     activation_fn=tf.nn.sigmoid)
-
-        return z
+        return z[:, 2:-2, 2:-2, :]
 
 
 def discriminator(x, reuse):
@@ -37,8 +35,7 @@ def discriminator(x, reuse):
                           activation_fn=leaky_relu)
 
         x = layers.flatten(x)
-        x = layers.fully_connected(x, num_outputs=1, activation_fn=None)
-        return x
+        return layers.fully_connected(x, num_outputs=1, activation_fn=None)
 
 
 with tf.name_scope('placeholders'):
@@ -53,23 +50,24 @@ d_generated = discriminator(x_generated, reuse=True)
 
 with tf.name_scope('loss'):
     g_loss = tf.reduce_mean(d_generated)
-    d_loss = tf.reduce_mean(d_true) - tf.reduce_mean(d_generated)
 
     epsilon = tf.random_uniform([], 0.0, 1.0)
     x_hat = epsilon * x_true + (1 - epsilon) * x_generated
     d_hat = discriminator(x_hat, reuse=True)
 
     ddx = tf.gradients(d_hat, x_hat)[0]
-    ddx = tf.sqrt(tf.reduce_sum(tf.square(ddx), axis=1))
-    ddx = tf.reduce_mean(tf.square(ddx - 1.0))
+    ddx = tf.sqrt(tf.reduce_sum(ddx ** 2, axis=1))
+    ddx = tf.reduce_mean((ddx - 1.0) ** 2)
 
-    d_loss = d_loss + 10 * ddx
+    d_loss = (tf.reduce_mean(d_true) - tf.reduce_mean(d_generated)) + 10 * ddx
 
 with tf.name_scope('optimizer'):
-    generator_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='generator')
-    train_generator = tf.train.AdamOptimizer(learning_rate=1e-4, beta1=0, beta2=0.9).minimize(g_loss, var_list=generator_vars)
-    discriminator_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='discriminator')
-    train_discriminator = tf.train.AdamOptimizer(learning_rate=1e-4, beta1=0, beta2=0.9).minimize(d_loss, var_list=discriminator_vars)
+    optimizer = tf.train.AdamOptimizer(learning_rate=1e-4, beta1=0, beta2=0.9)
+
+    g_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='generator')
+    g_train = optimizer.minimize(g_loss, var_list=g_vars)
+    d_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='discriminator')
+    d_train = optimizer.minimize(d_loss, var_list=d_vars)
 
 tf.global_variables_initializer().run()
 
@@ -81,9 +79,9 @@ for i in range(20000):
     images = batch[0].reshape([-1, 28, 28, 1])
     z_train = np.random.randn(50, 128)
 
-    session.run(train_generator, feed_dict={z: z_train})
+    session.run(g_train, feed_dict={z: z_train})
     for j in range(5):
-        session.run(train_discriminator, feed_dict={x_true: images, z: z_train})
+        session.run(d_train, feed_dict={x_true: images, z: z_train})
 
     if i % 100 == 0:
         print('iter={}/20000'.format(i))
